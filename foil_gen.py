9ba80ace7d4f5a6fbc18ef55895addb93159322e
marc22alain <marc@marcotypes.ca>
2016-06-02 16:31:49 -0400

    Now generating g-code, and saving it. Bugs are present.

--- foil_gen.py
+++ foil_gen.py
@@ -2,6 +2,8 @@ from Tkinter import *
 import ttk
 from math import *
 import MC_defaults as MC
+import simple_generators as SG
+import Glib as G
 
 
 
@@ -11,10 +13,11 @@ class Application(Frame):
         self.translate_X = 55
         self.translate_Y = 255
         self.options = {"grid_spacing": 50, "canW": 800, "canH": 500, "margin": 5}
+        self.gcode_generator = GCodeGenerator()
+        self.gcode_stage = ""
         Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
-        # self.drawFoil()
 
 
     def createWidgets(self):
@@ -135,12 +138,25 @@ class Application(Frame):
         self.max_cut_area_input.grid(row=row_num, column=1)
 
         row_num += 1
-        self.button_roughcutcode = Button(self.SubFrame,text="Generate rough cut code",command=self.drawRoughCut, width=30)
+        self.button_roughcutcode = Button(self.SubFrame,text="Generate rough cuts and code",command=self.drawRoughCut, width=30)
         self.button_roughcutcode.grid(row=row_num, column=0, columnspan=2, pady=5)
 
         row_num += 1
         ttk.Separator(self.SubFrame,orient=HORIZONTAL).grid(row=row_num, column=0, columnspan=2, sticky="ew", pady=5)
 
+        row_num += 1
+        self.file_name_label = Label(self.SubFrame, text='File name')
+        self.file_name_label.grid(row=row_num, column=0)
+        self.file_name_var = StringVar()
+        self.file_name_input = Entry(self.SubFrame, textvariable=self.file_name_var ,width=15)
+        self.file_name_input.grid(row=row_num, column=1)
+
+        row_num += 1
+        self.button_save_gcode_code = Button(self.SubFrame,text="Save g-code",command=self.saveGcode, width=30)
+        self.button_save_gcode_code.grid(row=row_num, column=0, columnspan=2, pady=5)
+
+        row_num += 1
+        ttk.Separator(self.SubFrame,orient=HORIZONTAL).grid(row=row_num, column=0, columnspan=2, sticky="ew", pady=5)
 
     def drawFoilChined(self):
         self.canvas.delete("profile")
@@ -217,6 +233,14 @@ class Application(Frame):
 
 
     def drawRoughCut(self):
+        """
+        This method runs in response to the "Generate rough cuts and code" button.
+        It must first wipe the slate clean before making new gcode.
+        """
+        self.gcode_generator.wipeClean()
+        self.gcode_generator.startProgram()
+        self.gcode_generator.setStockThickness(self.stock_thickness_var.get())
+
         self.canvas.delete("tool_cut")
         self.canvas.delete("stock")
 
@@ -266,23 +290,54 @@ class Application(Frame):
 
         self.canvas.itemconfig("tool_cut",  fill="#aa5555", outline="#883333")
 
+        self.gcode_generator.endProgram()
+
+        self.gcode_stage = "rough"
+
 
     def _drawRoughCut(self, x_right, y_bottom, x_left, y_top, x_prev):
         """
         Input arguments define the actual area to be cut in this pass.
+        Each call defines a new pass; should trigger a shift to the new X (or Y depending on stock layout in the machine) coordinate.
+        Each pass of the while loop should trigger a new depth of cut, i.e. a new shift in Z, and a new pass across the stock.
+        NOTE: an odd number of passes will leave the bit at the opposite end of the stock.
         """
+        # first make the gcode move to new pass X location... defined by the bit's center.
+        self.gcode_generator.shiftX( (x_right + x_left) / 2.0 )
+
         max_cut_area = self.max_cut_area_var.get() / 25.4**2 / self.section_width_var.get()**2
-        total_cut_area = (y_top - y_bottom) * (x_right - x_prev)
+        total_cut_area = (y_top - y_bottom) * ( min( x_right, self.stock_width_var.get() / self.section_width_var.get() ) - x_prev)
         assert total_cut_area > 0, "y_top %f, y_bottom %f, x_right %f, x_prev %f" % (y_top, y_bottom, x_right, x_prev)
         y_left = y_top - y_bottom
         y_incr = min(max_cut_area / total_cut_area, 1) * y_left
         y_prev_dest = y_top
         while y_left > 0:
-            # print "router pass, leaving %f" % total_cut_area
             y_dest = max(y_bottom, y_prev_dest - y_incr)
+            # now make the gcode for the pass across the stock
+            self.gcode_generator.shiftY( y_dest )
+            # now draw this pass
             self.canvas.create_rectangle(self.scal_X(x_right), self.neg_scal_Y(y_dest), self.scal_X(x_left), self.neg_scal_Y(y_prev_dest), tag="tool_cut")
             y_prev_dest = y_dest
             y_left -= y_incr
+
+
+    def saveGcode(self):
+        user_name = self.file_name_var.get()
+
+        foil_aspect = str(int( 100 * self.section_thickness_var.get() / self.section_width_var.get() ))
+        default_name =  "00" + foil_aspect + "_" + self.gcode_stage
+
+        if user_name == "":
+            file_name = default_name + ".ngc"
+        else:
+            file_name = user_name + ".ngc"
+
+        print self.gcode_generator.getGCode()
+        print
+        print "This g-code saved in file with name:", file_name
+
+        with open(file_name, 'w') as myFile:
+            myFile.write(self.gcode_generator.getGCode())
 
 
     def gridAdjust(self):
@@ -304,6 +359,63 @@ class Application(Frame):
 
     def neg_scal_Y(self, num):
         return - (num * self.scale) + self.translate_Y
+
+
+
+class GCodeGenerator(object):
+    def __init__(self):
+        self.gcode = ""
+        self.router_at_stock_start = True
+        self.stock_end = 100
+
+
+    def wipeClean(self):
+        self.gcode = ""
+        self.router_at_stock_start = True
+
+
+    def startProgram(self):
+        self.gcode += SG.startProgram(MC.default_feed_rate)
+        self.gcode += G.set_ABS_mode()
+
+    def setStockThickness(self, stock_thickness):
+        # More on the unit inconsistencies: must translate plot's inches to machine's metric
+        self.above_stock = stock_thickness * 25.4
+
+
+    def getGCode(self):
+        return self.gcode
+
+
+    def shiftX(self, new_x):
+        """
+        X refers to the plot X axis. Current assumption is that this will be a G0 in machine's Y.
+        new_x is an absolute coordinate.
+        """
+        # move in machine Z to just above the stock
+        self.gcode += G.G0_Z(self.above_stock)
+        # move to new X (or Y depending on stock layout in the machine)
+        self.gcode += G.G0_Y(new_x * 25.4)
+
+
+    def shiftY(self, new_y):
+        """
+        X refers to the plot X axis.
+        new_x is an absolute coordinate.
+        """
+        # move to new machine Z with G1
+        self.gcode += G.G1_Z(new_y * 25.4)
+        # make a cutting pass across the stock, assuming that this is in the machine's X axis
+        if self.router_at_stock_start == True:
+            self.gcode += G.G1_X(self.stock_end)
+        else:
+            self.gcode += G.G1_X(0)
+        # flip the stock end sentinel
+        self.router_at_stock_start = not self.router_at_stock_start
+
+    def endProgram(self):
+        self.gcode += SG.endProgram()
+
 
 
 app = Application()
